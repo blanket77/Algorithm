3bbdfa42c83186eaf26e2bb9169f3045c53d5445
Youchan Kim <70279365+blanket77@users.noreply.github.com>
2023-07-23 07:41:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    int num = n;
+    while(num){
+        answer += (num % 10); 
+        num /= 10;
+    }
+
+    return answer;
+}
