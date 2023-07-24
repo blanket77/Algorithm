d480e0e9ecb40ed5c60adbb26561ab43cd3646b0
Youchan Kim <70279365+blanket77@users.noreply.github.com>
2023-07-24 19:37:04 +0900

    [unrated] Title: 옹알이 (2), Time: 0.03 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -5,29 +5,23 @@ using namespace std;
 
 int solution(vector<string> babbling) {
     int answer = 0;
-    vector<string> two_word = {"ye","ma"};
-    vector<string> three_word = {"aya","woo"};
+    vector<string> word = {"ye","ma","aya","woo"};
 
     for(auto s : babbling){
         string back_word = "";
         int idx = 0;
         while(true){
             int current = idx;
-            for(auto t : two_word){
-                string tw = s.substr(idx,2);
+            for(auto t : word){
+                int t_len = t.length(); // 말할 수 있는 단어
+                string tw = s.substr(idx, t_len);
                 if(tw == t && tw != back_word){
-                    back_word = t;
-                    idx += 2;        
+                    back_word = t;   //  중복한 것은 배제
+                    idx += t_len;    //  말할 수 있는 단어 길이 만큼 sum    
                 }
             }
-            for(auto t : three_word){
-                string tw = s.substr(idx,3);
-                if(tw == t && tw != back_word){
-                    back_word = t;
-                    idx += 3;        
-                }
-            }
-            if(current == idx)
+
+            if(current == idx)  // 변화가 없다는 것은 말할 수 있는 단어가 없다는 뜻
                 break;
             if(idx == s.length()) {
                 answer++; 
