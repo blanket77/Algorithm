20b6d4ce5894373c8f08f446dd016a9ca7ebd8d2
Youchan Kim <70279365+blanket77@users.noreply.github.com>
2023-07-27 19:57:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    if('a' <= s[0] && s[0] <= 'z'){
+        s[0] = s[0] - 'a' + 'A' ; 
+    }
+    
+    for(int i = 1 ; i < s.size() ; i++){
+        s[i] = tolower(s[i]); 
+    }
+
+    for(int i = 1 ; i < s.size() ; i++){
+        char c = s[i];
+
+        if(c == ' ' && s[i+1] != ' ' && islower(s[i+1])){
+            s[i+1] = s[i+1] - 'a' + 'A';
+        }
+    }
+
+    answer = s;
+
+    return answer;
+}
