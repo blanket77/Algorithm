0e4a22f9a57dfd37d75c7277e4d55e3beaeba6a5
Youchan Kim <70279365+blanket77@users.noreply.github.com>
2023-08-11 13:41:19 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    sort(citations.begin(), citations.end(), greater<>());
+
+    for(int i = 0 ; i < citations.size(); i++){
+        if(citations[i] < i+1){
+            answer = i;
+            break;
+        }
+
+        if(i == citations.size()-1){
+            answer = citations.size();
+        }
+    }
+    
+
+    return answer;
+}
